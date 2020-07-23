6b4e13bf7722645d4b55b3e484dd67d6822993c8
Ahsan Zia <Ahsan97@hotmail.co.uk>
2020-07-23 18:47:24 +0100

    started using hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -10,17 +10,26 @@ import Display from './Display'
 import StartButton from './StartButton'
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer);
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()}/>
+                <Stage stage={stage} />
                 <aside>
-                    <div>
-                        <Display text="Score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
-                    <StartButton />
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
+                            <div>
+                                <Display text={`Score: ${score}`} />
+                                <Display text={`rows: ${rows}`} />
+                                <Display text={`Level: ${level}`} />
+                            </div>
+                        )}
+                    <StartButton callback={startGame} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
