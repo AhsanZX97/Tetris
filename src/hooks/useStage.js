b22c649544af39a710ba6a41675e4d7ab1facb0d
Ahsan Zia <Ahsan97@hotmail.co.uk>
2020-07-27 20:40:47 +0100

    working on set stage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -22,8 +22,15 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
-    }
-  }
+    };
+    setStage(prev => updateStage(prev));}, [
+      player.collided,
+      player.pos.x,
+      player.pos.y,
+      player.tetromino,
+      resetPlayer,
+    ]);
+
 
   return [stage, setStage];
 };
